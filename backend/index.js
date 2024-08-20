eb028646172dd17c2722ba7d66d25b1d57ce7c1a
Boi <2101140015@ms.hanu.edu.vn>
2024-08-20 17:11:44 +0700

    First commit

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.json());
+
+app.listen(port, (err) => {
+	if (!err) {
+		console.log(`App running successfully on port ${port}`);
+	} else {
+		console.log("fail to run server");
+	}
+});
